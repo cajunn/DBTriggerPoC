96107ae253fe20fcd9278deb10b93ee6fef2cd76
Marco.Leblanc2 <marco.leblanc2@novascotia.ca>
2025-07-15 15:42:33 -0300

    Add project files.

---
+++ DBTriggerTest/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+
+using DBTriggerTest.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace DBTriggerTest.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .ToTable(tb => tb.UseSqlOutputClause(false));
+        }
+    }   
+}
